e01bdeba8658573469bec6300c3a66cbf5f5b24d
Ayomide Oyekanmi <aoyekanmi@teamapt.com>
2020-04-09 22:30:20 +0100

    Cast all values to in

--- src/estimator.py
+++ src/estimator.py
@@ -26,38 +26,38 @@ def get_infections_by_requested_time(data, output):
 
 def get_severe_cases_by_requested_time(output):
     output['impact']['severeCasesByRequestedTime'] = \
-        0.15 * output['impact']['infectionsByRequestedTime']
+        int(0.15 * output['impact']['infectionsByRequestedTime'])
 
     output['severeImpact']['severeCasesByRequestedTime'] = \
-        0.15 * output['severeImpact']['infectionsByRequestedTime']
+        int(0.15 * output['severeImpact']['infectionsByRequestedTime'])
 
 
 def get_hospital_beds_by_requested_time(data, output):
-    available_beds = round(0.35 * data['totalHospitalBeds'], 1)
+    available_beds = 0.35 * data['totalHospitalBeds']
 
     impact_severe_cases = output['impact']['severeCasesByRequestedTime']
     output['impact']['hospitalBedsByRequestedTime'] = available_beds \
-        if available_beds >= impact_severe_cases else available_beds - impact_severe_cases
+        if available_beds >= impact_severe_cases else int(available_beds - impact_severe_cases)
 
     s_impact_severe_cases = output['severeImpact']['severeCasesByRequestedTime']
     output['severeImpact']['hospitalBedsByRequestedTime'] = available_beds \
-        if available_beds >= s_impact_severe_cases else available_beds - s_impact_severe_cases
+        if available_beds >= s_impact_severe_cases else int(available_beds - s_impact_severe_cases)
 
 
 def get_cases_for_icu_by_requested_time(output):
     output['impact']['casesForICUByRequestedTime'] = \
-        0.15 * output['impact']['infectionsByRequestedTime']
+        int(0.05 * output['impact']['infectionsByRequestedTime'])
 
     output['severeImpact']['casesForICUByRequestedTime'] = \
-        0.15 * output['severeImpact']['infectionsByRequestedTime']
+        int(0.05 * output['severeImpact']['infectionsByRequestedTime'])
 
 
 def get_cases_for_ventilators_by_requested_time(output):
     output['impact']['casesForVentilatorsByRequestedTime'] = \
-        0.02 * output['impact']['infectionsByRequestedTime']
+        int(0.02 * output['impact']['infectionsByRequestedTime'])
 
     output['severeImpact']['casesForVentilatorsByRequestedTime'] = \
-        0.02 * output['severeImpact']['infectionsByRequestedTime']
+        int(0.02 * output['severeImpact']['infectionsByRequestedTime'])
 
 
 def get_dollars_in_flight(data, output):
@@ -66,10 +66,10 @@ def get_dollars_in_flight(data, output):
     days = get_days(data['periodType'], data['timeToElapse'])
 
     impact_infections = output['impact']['infectionsByRequestedTime']
-    output['impact']['dollarsInFlight'] = impact_infections * population_perc * population_inc * days
+    output['impact']['dollarsInFlight'] = round(impact_infections * population_perc * population_inc * days, 2)
 
     s_impact_infections = output['severeImpact']['infectionsByRequestedTime']
-    output['severeImpact']['dollarsInFlight'] = s_impact_infections * population_perc * population_inc * days
+    output['severeImpact']['dollarsInFlight'] = round(s_impact_infections * population_perc * population_inc * days, 2)
 
     pass
 
